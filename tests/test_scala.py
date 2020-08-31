bb16954cb52ded04c3b82801e986d612b443a4e2
Maxime Kjaer <maxime.kjaer@gmail.com>
2020-08-31 22:42:53 +0200

    Add Scala test of default parameter expression

--- tests/test_scala.py
+++ tests/test_scala.py
@@ -31,3 +31,42 @@ def test_float_with_exponents(lexer):
         (Token.Text, '\n'),
     ]
     assert list(lexer.get_tokens(fragment)) == tokens
+
+def test_default_parameter(lexer):
+    fragment = "def f(using y: Char = if true then 'a' else 2): Int = ???\n"
+    tokens = [
+        (Token.Keyword, 'def'),
+        (Token.Text, ' '),
+        (Token.Name.Function, 'f'),
+        (Token.Punctuation, '('),
+        (Token.Keyword, 'using'),
+        (Token.Text, ' '),
+        (Token.Name, 'y'),
+        (Token.Operator, ':'),
+        (Token.Text, ' '),
+        (Token.Keyword.Type, 'Char'),
+        (Token.Text, ' '),
+        (Token.Operator, '='),
+        (Token.Text, ' '),
+        (Token.Keyword, 'if'),
+        (Token.Text, ' '),
+        (Token.Keyword.Constant, 'true'),
+        (Token.Text, ' '),
+        (Token.Keyword, 'then'),
+        (Token.Text, ' '),
+        (Token.Literal.String.Char, "'a'"),
+        (Token.Text, ' '),
+        (Token.Keyword, 'else'),
+        (Token.Text, ' '),
+        (Token.Literal.Number.Integer, '2'),
+        (Token.Operator, ')'),
+        (Token.Punctuation, ':'),
+        (Token.Text, ' '),
+        (Token.Keyword.Type, 'Int'),
+        (Token.Text, ' '),
+        (Token.Operator, '='),
+        (Token.Text, ' '),
+        (Token.Operator, '???'),
+        (Token.Text, '\n'),
+    ]
+    assert list(lexer.get_tokens(fragment)) == tokens
